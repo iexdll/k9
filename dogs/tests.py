9a35f96adc2d4d5ec819401750f853dc8c692716
Alexander Svidchenkov <a.svidchenkov@gmail.com>
2018-12-06 20:35:38 +0300

    Write test for dogs list

--- dogs/tests.py
+++ dogs/tests.py
@@ -34,7 +34,9 @@ class DogAPITestCase(TestCase):
     @classmethod
     def setUpTestData(cls):
         akita_inu = Breed.objects.create(name='Akita-inu')
+        golden_retriever_uuid = Breed.objects.create(name='Golden Retriever')
         cls.hachiko_uuid = Dog.objects.create(name='Hachi-ko', breed=akita_inu).uuid
+        cls.dug_uuid = Dog.objects.create(name='Dug', breed=golden_retriever_uuid).uuid
 
     def test_post_dog(self):
         response = self.client.post('/dog/', data='{"name": "Rex"}', content_type="application/json")
@@ -61,3 +63,10 @@ class DogAPITestCase(TestCase):
         response = self.client.delete('/dog/{0}/'.format(self.hachiko_uuid))
         self.assertEqual(response.status_code, 204)
         self.assertFalse(Dog.objects.filter(uuid=self.hachiko_uuid).exists())
+
+    def test_get_all_dogs(self):
+        response = self.client.get('/dogs/')
+        self.assertEqual(response.status_code, 200)
+        dogs_list = json.loads(response.content)
+        self.assertEqual(len(dogs_list), 2)
+        self.assertTrue(dogs_list[0])
