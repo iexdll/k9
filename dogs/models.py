801363510565242ec971f430625d08675b51d569
Alexander Svidchenkov <a.svidchenkov@gmail.com>
2018-12-06 19:25:22 +0300

    Extend Dog model to pass test

--- dogs/models.py
+++ dogs/models.py
@@ -1,4 +1,8 @@
+import uuid
 from django.db import models
+from breeds.models import Breed
 
 class Dog(models.Model):
-    pass
+    uuid = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    name = models.CharField(max_length=100, default='')
+    breed = models.ForeignKey(to=Breed, on_delete=models.PROTECT, null=True)
