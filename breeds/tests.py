102aa271bb0574454a5f7b837607bc4cabeab14d
Alexander Svidchenkov <a.svidchenkov@gmail.com>
2018-12-06 14:34:01 +0300

    Create basic breed model, and model test

--- breeds/tests.py
+++ breeds/tests.py
@@ -1,3 +1,11 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+from breeds.models import Breed
 
-# Create your tests here.
+class BreedsModelsTestCase(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        Breed.objects.create(name='Akita')
+
+    def test_breed_creation(self):
+        Breed.objects.create(name='Bulldog')
+        self.assertEqual(Breed.objects.count(), 2)
